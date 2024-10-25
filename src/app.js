88496c14d9c3a5a9b0b3a44117debd355ac768bc
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-10-25 12:18:09 +0530

    exploring get, post, use,delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 const express= require('express');
 const app= express();
 
-// app.use("/", (req, res) => {
-//     res.send("hello welome to dev tinder");
-// })
+//This will only handle Get call to /user
+app.get("/user", (req,res)=>{
+    res.send({
+        "FNamw":"Mohsin",
+        "LName": "Khan"
+    })
+
+})
+
+app.post("/user", (req,res) => {
+    res.send("Data successfully saved to the database")
+})
+app.delete("/user", (req,res) => {
+    res.send("Data deleted succesfully")
+})
+
+//This will mach all the HTTP method API call /test
 app.use("/test", (req, res) => {
     res.send("Heyy this is for the test");
+})
+app.use("/", (req, res) => {
+    res.send("hello welome to dev tinder");
 })
 
 app.listen(3000);
