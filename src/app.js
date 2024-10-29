a38815dfbd898c88feccd1ef470d670982d6a335
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-10-29 16:11:59 +0530

    validator + authentication

--- src/app.js
+++ src/app.js
@@ -2,31 +2,102 @@ const express= require('express');
 const connectDB = require("./config/database")
 const app= express();
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation")
+const bcrypt= require("bcrypt");
 
 app.use(express.json());
 
-
 //signup post api create
 app.post("/signup", async(req,res)=>{
+ 
+    try {   //validation of data
+validateSignUpData(req);
 
+const {firstName, lastName,emailId, password } = req.body;
+
+    //encrypt the password 
+
+    const passwordHash= await bcrypt.hash(password,10);
+    console.log(passwordHash);
    
 //creating new instance of the user models
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-   res.send("User Added succesfully");
-    } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message);
+    const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash,
+    });
+    
+    await user.save();
+    res.send("User Added succesfully");
+ } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
     }
-   
 });
 
-//update dat of the user
-app.patch("/user", async (req,res)=>{
-    const userId = req.body.userId;
-    const data = req.body;
+//login api
+app.post("/login", async (req,res)=>{
     try{
+        const {emailId, password} = req.body;
+
+        const user = await User.findOne({ emailId: emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid){
+            res.send("Login successFull!!!");
+        }
+        else{
+            throw new Error("Invalid credential");
+        }
+
+    } catch (err){
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
+//Feed api 
+app.get("/feed", async(req,res)=>{
+try{
+    const users = await User.find({});
+    res.send(users);
+} catch (err){
+    res.status(400).send("Something went wrong!");
+}
+});
+
+//Delete a user from the database
+app.delete("/user", async (req,res)=>{
+    const userid = req.body.userId;
+    try{
+        const user = await user.findByIdAndDelete({ _id:userId});
+        res.send("User deleted successfullt");
+    } catch (err){
+        res.status(400).send("something went wrong!");
+    }
+})
+
+//update dat of the user
+app.patch("/user/:userId", async (req,res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+
+   
+    try{
+        const ALLOWED_UPDATES =[
+             "photourl", "about", "gender", "age", "skills"
+          ]
+      
+          const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k)
+      );
+      if(!isUpdateAllowed){
+          throw new Error("Update not allowed");
+      }
+      if (data?.skills.length >10){
+        throw new Error("skills cannot be more than 10");
+      }
         const user = await User.findByIdAndUpdate({ _id: userId}, data, {
             returnDocument: "after",
             runValidator: true,
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
+const validator=require("validator");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: true,
-        minLength:4,
+        minLength:2,
         maxLength:50,
     },
     lastName: {
@@ -16,10 +16,20 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique: true,
         trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address:" + value);
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value) {
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a Strong Password:" + value);
+            }
+        }
     },
     age: {
         type: Number,
@@ -36,6 +46,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://www.dgvaishnavcollege.edu.in/dgvaishnav-c/uploads/2021/01/dummy-profile-pic.jpg",
+        validate(value) {
+            if(!validator.isURL(value)){
+                throw new Error("Invalid Photo URL:" + value);
+            }
+        }
     },
     about: {
         type: String,
